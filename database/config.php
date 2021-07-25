1afcedbbf6601b5c1735bdc01c921f78b98b8006
Svetlana-gach <77901136+Svetlana-gach@users.noreply.github.com>
2021-07-25 15:46:27 +0300

    Add files via upload

---
+++ database/config.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+	"database"	=>	[
+		"database"	=> "new_base",
+		"username"	=> "root",
+		"password"	=> "root",
+		"connection"=> "mysql:host=localhost",
+		"charset"	=> "utf8"
+	],
+
+]; 
+
+?>
